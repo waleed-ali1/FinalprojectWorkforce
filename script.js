27096a2cfe9996fe54aee13c2ec0672fa3effcd0
walid <walidkenaan10@gmail.com>
2021-09-12 23:18:47 +0300

    hovering the icons using jquery

---
+++ script.js
@@ -0,0 +1,61 @@
+ 
+$(document).ready(function(){
+    $("#instagram").hover(function(){
+        $(this).css("width", "150px");
+    }, function(){ 
+         $(this).css("width", "100px");
+   
+    });
+});
+
+
+$(document).ready(function(){
+    $("#instagram").hover(function(){
+        $(this).css("height", "150px");
+    }, function(){ 
+         $(this).css("height", "100px");
+   
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#github").hover(function(){
+        $(this).css("width", "150px");
+    }, function(){ 
+         $(this).css("width", "100px");
+   
+    });
+});
+
+
+$(document).ready(function(){
+    $("#github").hover(function(){
+        $(this).css("height", "150px");
+    }, function(){ 
+         $(this).css("height", "100px");
+   
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#linkedin").hover(function(){
+        $(this).css("width", "150px");
+    }, function(){ 
+         $(this).css("width", "100px");
+   
+    });
+});
+
+
+$(document).ready(function(){
+    $("#linkedin").hover(function(){
+        $(this).css("height", "150px");
+    }, function(){ 
+         $(this).css("height", "100px");
+   
+    });
+});
