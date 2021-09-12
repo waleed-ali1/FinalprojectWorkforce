cd13e7cd95733b209bde79e0c2be412bbbf31c30
walid <walidkenaan10@gmail.com>
2021-09-12 23:49:24 +0300

    adding a button and a function to get data from github

--- script.js
+++ script.js
@@ -59,3 +59,15 @@ $(document).ready(function(){
    
     });
 });
+
+
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("requestDate").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://api.github.com/users/waleed-ali1/repos", true);
+    xhttp.send();
+  }
